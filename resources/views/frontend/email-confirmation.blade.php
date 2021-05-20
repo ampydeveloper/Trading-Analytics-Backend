af491943ad3b05704882ab09f85da79a2272336f
ampydeveloper <manpreet.luminoguruz@gmail.com>
2021-05-21 03:50:12 +0530

    sprint 1.5 latest

--- resources/views/frontend/email-confirmation.blade.php
+++ resources/views/frontend/email-confirmation.blade.php
@@ -2,14 +2,46 @@
 
 @section('title', app_name() . ' | ' . __('navs.general.home'))
 
+@push('after-styles')
+<style type="text/css">
+    .navbar {
+        display: none;
+    }
+    .outer-green{
+        background: #1ce783;
+        padding: 70px 0;
+    }
+    .align-cen{
+        margin: 0 auto;
+    }
+    .inner-white{
+        background: #fff;
+        padding: 50px;
+        text-align: center;
+    }
+    .message-outer{
+        margin: 0;
+        line-height: 4;
+        font-weight: bolder;
+        font-size: 16px;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+    }
+</style>
+@endpush
+
 @section('content')
-<div class="">
-    <div class="message-outer alert alert-success">
-        <?php
-        echo $message;
-        ?>
+<div class="row outer-green">
+    <div class="col-sm-10 align-cen">
+        <div class="inner-white">
+            <div class="message-outer ">
+                <?php
+                echo $message;
+                ?>
+            </div>
+        </div>
     </div>
-</div><!--row-->
+</div>
 <script type="text/javascript">
     setTimeout(function () {
         location.href = '<?php echo env('VUE_URL'); ?>';
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -10,7 +10,8 @@
             <meta name="csrf-token" content="{{ csrf_token() }}">
             <title>@yield('title', app_name())</title>
             <!--<meta name="description" content="@yield('meta_description', '')">-->
-            <meta name="author" content="@yield('meta_author', 'LOC')">
+            <meta name="author" content="@yield('meta_author', '')">
+            <link rel="shortcut icon" type="image/jpg" href="/slabstox.ico"/>
             @yield('meta')
 
             @stack('before-styles')
@@ -24,8 +25,7 @@
                 @include('includes.partials.logged-in-as')
                 @include('frontend.includes.nav')
 
-                <div class="container">
-            
+                <div class="container-fluid">
                     @yield('content')
                 </div><!-- container -->
             </div><!-- #app -->
